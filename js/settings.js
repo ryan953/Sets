0f8552577c0243309c478350581e2be6b3b229e7
Ryan Albrecht <ryan@ryanalbrecht.ca>
2012-04-16 22:48:55 -0400

    refactor settings to be a better candidate for strict mode.

--- js/settings.js
+++ js/settings.js
@@ -1,50 +1,52 @@
+/*global getStore:false */
 (function($) {
+	"use strict";
+	var store = getStore();
+
 	window.Settings = {
 		selectedMode: function() {
-			return $('#settings-easy-mode').is(':checked') ? 'easy' : 'regular';
+			return (store.get('settings-easy-mode') ? 'easy' : 'regular');
 		},
 
 		helpMode: function() {
-			return $('#settings-help-mode').is(':checked');
+			return (store.get('settings-help-mode'));
 		}
 	};
-})(jQuery);
 
-$(document).ready(function() {
-	var store = getStore();
-
-	// settings -> needs ref to the store
-	$('[id^=settings]').bind('change', function() {
-		var self = $(this),
-			key = self.prop('id'),
-			value = self.val();
-		if (self.filter('[type=checkbox]')) {
-			value = self.prop('checked');
-		}
-		// console.debug('writing', key, value);
-		store.put(key, value);
-	}).each(function() {
-		var self = $(this),
-			key = self.prop('id'),
-			value = store.get(key);
-		// console.debug('read', key, value);
-		if (value !== null) {
+	$(document).ready(function() {
+		// settings -> needs ref to the store
+		$('[id^=settings-]').bind('change', function() {
+			var self = $(this),
+				key = self.prop('id'),
+				value = self.val();
 			if (self.filter('[type=checkbox]')) {
-				self.prop('checked', value);
-			} else {
-				self.val(value);
+				value = self.prop('checked');
 			}
-		}
-	});
+			// console.debug('writing', key, value);
+			store.put(key, value);
+		}).each(function() {
+			var self = $(this),
+				key = self.prop('id'),
+				value = store.get(key);
+			// console.debug('read', key, value);
+			if (value !== null) {
+				if (self.filter('[type=checkbox]')) {
+					self.prop('checked', value);
+				} else {
+					self.val(value);
+				}
+			}
+		});
 
-	// jquery plugin for '.scoreboard-display .time' is needed
-	$('.scoreboard-display [data-scoreboard-display]').bind('click', function() {
-		var elem = $(this);
-		var visible = elem.hide().next('[data-scoreboard-display]').show();
-		if (!visible.length) {
-			visible = elem.siblings('[data-scoreboard-display]').first().show();
-		}
-		store.put('scoreboardDisplay', visible.data('scoreboardDisplay'));
-		return false;
+		// jquery plugin for '.scoreboard-display .time' is needed
+		$('.scoreboard-display [data-scoreboard-display]').bind('click', function() {
+			var elem = $(this);
+			var visible = elem.hide().next('[data-scoreboard-display]').show();
+			if (!visible.length) {
+				visible = elem.siblings('[data-scoreboard-display]').first().show();
+			}
+			store.put('scoreboardDisplay', visible.data('scoreboardDisplay'));
+			return false;
+		});
 	});
-});
+})(jQuery);
