2239f2ea8976ecbe1f482d50f81addbdc0911660
Ryan Albrecht <ryan@ryanalbrecht.ca>
2012-04-24 20:28:30 -0400

    update qunit and include a composite file.

---
+++ test/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Sets! QUnit Test Suite</title>
+	<link rel="stylesheet" href="../lib/qunit.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="qunit-composite.css">	
+	<script src="../lib/qunit.js"></script>
+	<script src="qunit-composite.js"></script>
+
+	<script>
+	QUnit.testSuites([
+		"./logic.html",
+		"./logic-setsui.html",
+		"./assistant.html"
+   	]);
+	</script>
+</head>
+<body>
+	<div id="qunit"></div>
+	<div id="qunit-fixture">
+</div>
+</body>
+</html>
