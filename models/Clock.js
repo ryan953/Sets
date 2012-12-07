b2d493b1cbb57a6beda128b3a0447f6fc58f7895
Ryan Albrecht <ryan@ryanalbrecht.ca>
2012-12-06 23:04:45 -0500

    Re-added the Clock class as a backbone model with tests.

---
+++ models/Clock.js
@@ -0,0 +1,42 @@
+/*jshint smarttabs:true */
+/*global _ Backbone setTimeout clearTimeout */
+
+window.Clock = (function(Parent) {
+	"use strict";
+
+	return Parent.extend({
+		initialize: function(tickAction, delay) {
+			var _this = this;
+			_this.delay = delay;
+			_this.clockTick = function() {
+				if (tickAction()) {
+					_this.trigger('clock.tick', _this);
+					_this.timer = setTimeout(_this.clockTick, _this.delay);
+				} else {
+					_this.stop();
+				}
+			};
+		},
+
+		/**
+		 * Takes a delay between clock ticks, and an action to do each tick
+		 * when the action returns false the clock will turn itself off
+		 */
+		start: function() {
+			this.stop();
+			this.timer = setTimeout(this.clockTick, this.delay);
+			this.trigger('clock.start', this);
+		},
+
+		/**
+		 * Stop the timer so we don't keep running anything
+		 */
+		stop: function() {
+			if (this.timer) {
+				clearTimeout(this.timer);
+				this.timer = null;
+				this.trigger('clock.stop', this);
+			}
+		}
+	});
+})(Backbone.Model);
---
+++ test/models/clock.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Test: clock.js</title>
+<link rel="stylesheet" href="../../lib/qunit.css" type="text/css" media="screen" />
+<script src="../../lib/qunit.js"></script>
+<script src="../../lib/jquery-1.7.2.min.js"></script>
+<script src="../../lib/underscore-1.4.2.min.js"></script>
+<script src="../../lib/backbone-0.9.2.min.js"></script>
+
+<script src="../../models/Clock.js"></script>
+
+<script>
+"use strict";
+
+$(document).ready(function () {
+	// module('Clock::factory');
+	// test('requires `delay` param', function() {
+	// 	raises(function() { new Assistant(); }, Error, 'delay param is required');
+	// });
+
+	module('Clock', {
+		setup: function() {
+			this.delay = 50;
+		}
+	});
+
+	test('clock triggers event on start', function() {
+		var clock = new Clock(function() {
+				return false;
+			}, this.delay);
+		expect(1);
+		stop(1);
+
+		clock.on('clock.start', function() {
+			ok(true, 'clock.start event caught');
+			start();
+		});
+		clock.start();
+	});
+
+	test('clock triggers event on stop', function() {
+		var clock = new Clock(function() {
+				return false;
+			}, this.delay);
+		expect(1);
+		stop(1);
+
+		clock.on('clock.stop', function() {
+			ok(true, 'clock.stop event caught');
+			start();
+		});
+		clock.start();
+	});
+
+	test('clock ticks 3 times', function() {
+		var count = 3,
+			clock = new Clock(function() {
+				ok(true, 'Clock tick ' + count);
+				start();
+				return count--;
+			}, this.delay);
+		expect(count);
+		stop(count);
+
+		clock.start();
+	});
+
+	test('clock can be stopped early', function() {
+		var count = 3,
+			clock = new Clock(function() {
+				ok(false, 'Should not have a chance to tick');
+				return count--;
+			}, this.delay);
+		expect(2);
+		stop(1);
+
+		clock.on('clock.start', function() {
+			ok(true, 'clock.start event caught');
+		});
+		clock.on('clock.stop', function() {
+			ok(true, 'clock.stop event caught');
+			start();
+		});
+		clock.start();
+		clock.stop();
+	});
+});
+</script>
+
+</head>
+<body>
+	<div id="qunit"></div>
+	<div id="qunit-fixture">test markup, will be hidden</div>
+</body>
+</html>
