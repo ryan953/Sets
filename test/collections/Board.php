08436fb03d914179ebb8a724dc2a636c765d4845
Ryan Albrecht <ryan@ryanalbrecht.ca>
2013-05-05 11:40:40 -0400

    Removed dead test code.

--- test/collections/Board.php
+++ test/collections/Board.php
@@ -17,54 +17,6 @@
 "use strict";
 
 $(document).ready(function () {
-	module('Collections.Board');
-	// test('Creating a card', function() {
-	// 	var c = new Models.Card();
-	// 	ok(c, "new card created successfully");
-	// });
-
-	// test('Card.factory() builds a card', function() {
-	// 	var props = {
-	// 		count: 2,
-	// 		shape: 'Oval',
-	// 		fill: 'solid',
-	// 		color: 'blue'
-	// 	};
-	// 	var card = new Card(2, 'Oval', 'solid', 'blue');
-	// 	deepEqual(Card.factory(props), card, 'Factory can build cards correctly');
-
-	// });
-
-	// test('isEmpty works correctly', function () {
-	// 	ok(new Card().isEmpty(), "A card with no params IS empty");
-	// 	ok(new Card( 1, "Oval", "striped").isEmpty(), "A card with only 3 params IS empty");
-
-	// 	ok(!(new Card( 1, "Oval", "striped", "green").isEmpty()), "A card with all 4 params is NOT empty");
-	// });
-
-	// test('select/deselect', function() {
-	// 	var c = new Card();
-
-	// 	c.select();
-	// 	ok(c.isSelected, 'card gets selected');
-	// 	c.deselect();
-	// 	ok(!c.isSelected, 'card gets UNselected');
-	// });
-
-	// test('getClassAttr', function() {
-	// 	Card.classNameMap.attr_one = 'class-one';
-	// 	Card.classNameMap.attr_two = 'class-two';
-
-	// 	var c = new Card();
-	// 	equal('', c.getClassAttr(), 'should have no clasname');
-
-	// 	c.attr_one = true;
-	// 	equal('class-one', c.getClassAttr(), 'should now have a classname');
-
-	// 	c.attr_two = true;
-	// 	equal('class-one class-two', c.getClassAttr(), 'should have two classnames');
-	// });
-
 	module('Collection.Board::isASet');
 	test('isASet is detecting correctly', function() {
 		var diff_color = [
