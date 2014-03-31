15a7823299fbe78bdd10667388bda458ac36ea51
Ryan Albrecht <ryan@ryanalbrecht.ca>
2014-03-31 00:48:11 -0400

    Remove the generated todo application

--- js-todo/collection.js
+++
@@ -1,5 +0,0 @@
-define(['thorax'], function (Thorax) {
-  return Thorax.Collection.extend({
-
-  });
-});
--- js-todo/view.js
+++
@@ -1,5 +0,0 @@
-define(['thorax'], function (Thorax) {
-  return Thorax.View.extend({
-
-  });
-});
